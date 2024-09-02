52a7fe3cea692616b71c0bafae1b5f1302e55b73
ZcvGuilherme <169081282+ZcvGuilherme@users.noreply.github.com>
2024-09-02 17:56:47 -0300

    bruh

--- main.py
+++ main.py
@@ -1,24 +1,43 @@
-import tkinter as tk
+from tkinter import *
 import Funcoes
 from Funcoes import Init_Baixar
+class PlaceholderEntry(Entry):
+    def __init__(self, master=None, placeholder="", *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.placeholder = placeholder
+        self.bind("<FocusIn>", self._on_focus_in)
+        self.bind("<FocusOut>", self._on_focus_out)
+        self._set_placeholder()
 
+    def _set_placeholder(self):
+        self.insert(0, self.placeholder)
+        self.config(fg='grey', font=('Arial', 15))
+
+    def _on_focus_in(self, event):
+        if self.get() == self.placeholder:
+            self.delete(0, END)
+            self.config(fg='black', font=('Arial', 15))
+
+    def _on_focus_out(self, event):
+        if self.get() == '':
+            self._set_placeholder()
 
 # Configuração da janela principal
-root = tk.Tk()
+root = Tk()
 root.title("Uploader de Arquivos")
 root.geometry("450x250")
 
-# Rótulo de instrução
-label = tk.Label(root, text="Clique no botão para fazer upload de um arquivo.")
-label.pack(pady=20)
+choose_file = Button(root, command=Funcoes.upload_file)
+choose_file.place(relx=0.1, rely=0.1, relheight=0.15, relwidth=0.1)
 
-# Botão para abrir o upload do deck
-upload_deck = tk.Button(root, text="Upload de Arquivo do deck", command=Funcoes.upload_file)
-upload_deck.pack(pady=10)
+choose_file_path = PlaceholderEntry(root, placeholder='Informe o arquivo...')
+choose_file_path.place(relx=0.23, rely=0.1, relheight=0.15, relwidth=0.7)
 
-#Botão de acessar o Historico
-historico = tk.Button(root,text="Historico")
+nome_arquivo = PlaceholderEntry(root, placeholder='Digite o nome do arquivo...')
+nome_arquivo.place(relx=0.23, rely=0.35, relheight=0.15, relwidth=0.7)
+
+botao_criar = Button(root, text='Criar', font=('Arial', 15, 'bold'), command=lambda: criar_arquivo(choose_file, nome_arquivo))
+botao_criar.place(relx=0.35, rely=0.7, relheight=0.2, relwidth=0.3)
 
 
-# Inicia o loop principal da interface
 root.mainloop()
