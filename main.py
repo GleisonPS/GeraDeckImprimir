034a44994e67894bd1889b74c32b381c32a9ec61
GleisonPS <gleisonp881@gmail.com>
2024-09-08 10:32:03 -0300

    Otimizando o código

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from tkinter import *
 from Funcoes import *
 
-
-
 # Configuração da janela principal
 root = Tk()
 root.title("Uploader de Arquivos")
 root.geometry("450x250")
+
+#Campo pra informa o caminho doarquivo do deck .ydk
 imagem = PhotoImage(file='Imgs/pasta(3).png')
 choose_file = Button(root, command=lambda: abrir_pasta(choose_file_path,nome_arquivo), image=imagem)
 choose_file.place(relx=0.1, rely=0.1, relheight=0.15, relwidth=0.1)
@@ -17,7 +17,7 @@ choose_file_path.place(relx=0.23, rely=0.1, relheight=0.15, relwidth=0.7)
 nome_arquivo = PlaceholderEntry(root, placeholder='Digite o nome do arquivo...')
 nome_arquivo.place(relx=0.23, rely=0.35, relheight=0.15, relwidth=0.7)
 
-botao_criar = Button(root, text='Criar', font=('Arial', 15, 'bold'), command=lambda: criar_deck(choose_file_path,nome_arquivo)) #(choose_file, nome_arquivo))
+botao_criar = Button(root, text='Criar', font=('Arial', 15, 'bold'), command=lambda: criar_deck(choose_file_path,nome_arquivo)) 
 botao_criar.place(relx=0.35, rely=0.7, relheight=0.2, relwidth=0.3)
 
 
